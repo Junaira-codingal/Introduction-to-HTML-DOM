faaf8e033072f661b284236ff7c35c87b0e2c306
Junaira <advshakerullah@gmail.com>
2025-03-29 10:46:20 +0600

    dfg

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
 function validate(e) {
   e.preventDefault();
 
+  const fullName = document.getElementById("fullName").value;
   const email = document.getElementById("email").value;
-
   const pass = document.getElementById("password").value;
-
+  const confpass = document.getElementById("confirmPassword").value;
   const age = document.getElementById("age").value;
 
   const msgBox = document.getElementById("message");
-
   let message = "";
 
   if (email === "") {
@@ -17,12 +16,19 @@ function validate(e) {
   } else if (pass === "") {
     message = "Password Must be Atleast 8 Characters long";
     msgBox.style.color = "red";
-  } else if (age === "") {
-    message = "Age Must be Between 12 - 50";
+  } else if (confpass === "") {
+    message = "Confirm Password must be entered";
+    msgBox.style.color = "red";
+  } else if (pass !== confpass) {
+    message = "Passwords do not match!! Try Again";
+    msgBox.style.color = "red";
+  } else if (age == "") {
+    message = "Age Must be Between 12 and 50";
     msgBox.style.color = "red";
   } else {
-    message = "Login Succesful";
+    message = "Account Created Successfully! Redirecting to Login...";
     msgBox.style.color = "green";
+    alert("Account Created Successfully! Back To the Login");
   }
 
   msgBox.innerText = message;
